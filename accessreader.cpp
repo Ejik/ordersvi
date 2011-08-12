17440701968ed2fd8b9d4c1d8c08864aae7e55f6
Sergii Kiselev <sergii.kiselev@gmail.com>
2011-08-13 01:25:24 +0400

    accessreader работаетс базой не совсем корректно

--- accessreader.cpp
+++ accessreader.cpp
@@ -1,8 +1,12 @@
 #include <QDebug>
+#include <QSqlError>
 #include "accessreader.h"
 #include <QtGlobal>
 #include <QtSql/QSqlDatabase>
+#include <QSqlQuery>
 #include <QDir>
+#include <QDate>
+#include <QVariant>
 
 AccessReader::AccessReader()
 {
@@ -14,12 +18,34 @@ IndicatorsModel AccessReader::getData() {
 
     if (createConnection()) {
 
-        qsrand(10000);
+        QDate currentDate = QDate::currentDate();
+        QString month = QString::number(currentDate.month());
+        QString year = QString::number(currentDate.year());
+        QString date1 = "#1\\1\\2010#"; //"#" + month + "\\1\\" + year + "#";
+        QString date2 = "#1\\4\\2010#"; //"#" + month + "\\" + QString::number(currentDate.daysInMonth())+ "\\" + year + "#";
 
-        model.setAmount(qrand());
-        model.setSum(qrand());
-        model.setCash(qrand());
+        QSqlQuery query;
 
+        QString sql = "SELECT COUNT(DH2457.SP2451) AS Amount, SUM(DH2457.SP2451) AS Summa "
+                "FROM DH2457 LEFT JOIN _1SJOURN AS Journal ON DH2457.IDDOC=Journal.IDDOC "
+                "WHERE (((Journal.Date) Between ?date1 AND ?date2))";
+        sql = "SELECT Amount, Summa FROM Zapros";
+
+        query.prepare(sql);
+
+        query.bindValue(":date1", date1);
+        query.bindValue(":date2", date2);
+
+        if (query.exec(sql)) {
+
+            query.next();
+
+            model.setAmount(query.value(0).toInt());
+            model.setSum(query.value(1).toInt());
+            model.setCash(0);
+        } else {
+            qDebug() << query.lastError();
+        }
     }
     return model;
 }
